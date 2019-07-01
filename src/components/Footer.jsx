7ac97a3d7d13a9467666c3268077b1749a70e226
Makeespresso <makeespresso@gmail.com>
2019-06-30 21:08:14 -0400

    Style Articles Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
       <h1 className="logo">9muses</h1>
       <div className="social-media-footer">
         <ul>
-          <li><a href="https://github.com/" target="_blank"><img src="img/github.png" id="socialImg" alt="GitHub"></img></a></li>
-          <li><a href="https://github.com/" target="_blank"><img src="img/twitter.png" id="socialImg" alt="Twitter"></img></a></li>
-          <li><a href="https://github.com/" target="_blank"><img src="img/envelope.png" id="socialImg" alt="Envelope"></img></a></li>
-          <li><a href="https://github.com/" target="_blank"><img src="img/linkedin.png" id="socialImg" alt="LinkedIn"></img></a></li>
-          <li><a href="https://github.com/" target="_blank"><img src="img/makeespresso.png" id="socialImg" alt="Makeespresso"></img></a></li>
-          <li><a href="https://github.com/" target="_blank"><img src="img/pinterest.png" id="socialImg" alt="Pinterest"></img></a></li>
+          <li><a href="https://github.com/makeespresso" target="_blank"><img src="img/github.png" id="socialImg" alt="GitHub"></img></a></li>
+          <li><a href="https://twitter.com/makeespresso" target="_blank"><img src="img/twitter.png" id="socialImg" alt="Twitter"></img></a></li>
+          <li><a href="mailto:makeespresso@gmail.com" target="_blank"><img src="img/envelope.png" id="socialImg" alt="Envelope"></img></a></li>
+          <li><a href="https://www.linkedin.com/in/anasbenedict/" target="_blank"><img src="img/linkedin.png" id="socialImg" alt="LinkedIn"></img></a></li>
+          <li><a href="http://makeespresso.co/" target="_blank"><img src="img/makeespresso.png" id="socialImg" alt="Makeespresso"></img></a></li>
+          <li><a href="https://www.pinterest.com/" target="_blank"><img src="img/pinterest.png" id="socialImg" alt="Pinterest"></img></a></li>
         </ul >
       </div >
     </footer >
